8cf2a85b332672c2a0f434b7b8777521319d04ee
ruansaure17 <167703052+ruansaure17@users.noreply.github.com>
2024-07-12 22:09:49 -0300

    Update auxiliar.py

--- auxiliar.py
+++ auxiliar.py
@@ -2,11 +2,5 @@ import pyautogui as saure
 import os
 from time import sleep
 
-#sleep(5)
-#print(saure.position())
-
-#with open("senha_rotta.txt","w") as arq:
-#   arq.senha = "senha"
-
-import site
-print(site.getsitepackages())
+sleep(3)
+print(saure.position())
